ec85ef3d32e0f67f370a4677c218c0c54bb83090
Mikita Kryvitski <solairerove@gmail.com>
2019-05-23 22:24:28 +0100

    feature: init brand new single view project

---
+++ Stack Layout Practice iOS12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stack Layout Practice iOS12
+//
+//  Created by Nikita Krivitski on 5/23/19.
+//  Copyright © 2019 Nikita Krivitski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
